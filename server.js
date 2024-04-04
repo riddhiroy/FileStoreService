6e36137e664f7ddadf7268cad6f1f95b54f6bcb2
Riddhi Roy <riddhiroy@yahoo.in>
2024-04-04 07:33:00 +0530

    integrating GridFs for file CRUD in MongoDB

--- server.js
+++ server.js
@@ -1,63 +1,94 @@
 const Joi = require('joi');
 const express = require('express');
 const mongoose = require('mongoose')
-const fileStore = require('../FileStoreService/Models/fileStoreModel')
+const bodyParser = require('body-parser')
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage').GridFsStorage;
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+const { name } = require('ejs');
 
 const app = express();
-app.use(express.json())
 
-mongoose.connect('mongodb+srv://riddhiroy2000:riddhiroypassword@mongoclusterriddhi.ljucxap.mongodb.net/FileStore?retryWrites=true&w=majority')
-    .then(() => {
-        console.log('MongoDB connected...');
-    }).catch(err => {
-    console.error('MongoDB connection error:', err);
-});
-
-let files = new Map();
+// Middleware 
+app.use(bodyParser.json())
+app.use(express.json());
+app.use(methodOverride('_method'))
 
 
-app.get('/', (req, res) => {
-    res.send('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>File Store App</title></head><body><div><h1>Home page</h1><p>Select an action to execute for File Store App.</p><p>Choose from:</p><ul><li>Add: <code>/add</code></li><li>Remove: <code>/rmv</code></li><li>List: <code>/ls</code></li></ul></div></body></html>')
+app.set('view engine' , 'ejs');
+
+
+// MongoDB URI
+const mongoURI = 'mongodb+srv://riddhiroy2000:riddhiroypassword@mongoclusterriddhi.ljucxap.mongodb.net/FileStore?retryWrites=true&w=majority'
+
+// MongoDB Connection
+const conn = mongoose.createConnection(mongoURI)
+
+let gfs
+conn.once('open', () => {
+    gfs = Grid(conn.db, mongoose.mongo)
+    gfs.collection('uploads')
 })
 
-app.post('/add', (req, res) => {
-    //handle n/w interruptions.
-    const schema = Joi.object({
-        name: Joi.string().required(),
-        content: Joi.string()
-    })
-    const result = schema.validate(req.body)
-    if(result.error) {
-        //400 Bad Request
-        res.status(400).send(result.error.details[0].message)
-        return
+// Create storage engine
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            const filename = file.originalname
+            const fileInfo = {
+                filename: filename,
+                bucketName: 'uploads'
+            }
+            resolve(fileInfo)
+        })
     }
-    if(fileStore.findOne({"name":req.body.name})!=null){
-        //400 Bad Request
-        res.status(400).send('Error: File already exists in store.')
-        return
+});
+
+// Set destination for storing uploaded files
+
+const upload = multer({
+    storage: storage,
+    // Filter file uplaod based on request method
+    fileFilter: async function (req, file, callback) {
+        const existingFile = await getFileByFilename(file.originalname)
+        if(existingFile) {
+            if (req.method === 'POST')
+                return callback(`Error: File upload attempted for an already existing file ${file.originalname}`)
+            else if (req.method === 'PUT') {
+                const isDeleted = await deleteFileByFilename(file.originalname)
+                console.log(isDeleted)
+                return callback(null, true)
+            }
+        }
+        if(req.method === 'PUT'){
+            console.log(`File did not exist in store. Created file ${file.originalname}`);
+        }
+        callback(null, true)
     }
-    const file = new fileStore(req.body)
-    file.save().then(result => {
-        console.log(result)
-        res.send(`Added file ${file.name}`)
-    }).catch(err => {
-        console.log(err.message)
-        res.send(`Error ${err.message}`)
-    })
+})
+
+app.post('/add', upload.single('file'), async (req, res) => {
+    console.log("File Added:\n", {file: req.file})
+    res.send(`Added file ${req.file.originalname} to the file store.`)
 })
 
 app.get('/ls', async (req, res) => {
-    try{
-        const fileList = await fileStore.find({},{__v: 0}) //query,projection
-        res.send(fileList)
-    } catch (error){
-        console.log(err.message)
-        res.send(`Error ${err.message}`)
+    try {
+        let files = await gfs.files.find().toArray();
+        
+        // Extract filenames from the files array
+        let filenames = files.map(file => file.filename);
+
+        // Send the array of filenames in the response
+        res.send(filenames);
+    } catch (err) {
+        res.json({err})
     }
 })
 
-app.put('/rm', async (req, res) => {
+app.delete('/rm', async (req, res) => {
     const schema = Joi.object({
         name: Joi.string().required()
     })
@@ -68,54 +99,27 @@ app.put('/rm', async (req, res) => {
         return
     }
     try {
-        let file = await fileStore.findOne({"name": req.body.name});
-        const isFileAbsent = !file
-        if(isFileAbsent){
-            //400 Bad Request
+        const file = await gfs.files.findOne({ filename: req.body.name });
+        // check if file exists in file store
+        if(!file){
             res.status(400).send('File does not exist in the file store.')
             return
         }
-        const result = await fileStore.findOneAndDelete({ "name" : req.body.name })
-        res.send(`File removed successfully from store. File: ${req.body.name}`)
+        const gsfb = new mongoose.mongo.GridFSBucket(conn.db, { bucketName: 'uploads' });
+        gsfb.delete(file._id);
+        return res.status(200).send(`File successfully removed from store.`)  
     } catch (error) {
         console.log(error.message)
-        res.send(`Error ${error.message}`)
+        res.status(404).send(`Error ${error.message}`)
     }
 })
 
-app.put('/update', async (req, res) => {
+app.put('/update', upload.single('file'), async (req, res) => {
 
     //TODO: skip sending contents if the server already has it
-    const schema = Joi.object({
-        name: Joi.string().required(),
-        content: Joi.string().required()
-    })
-    const result = schema.validate(req.body)
-    if(result.error) {
-        //400 Bad Request
-        res.status(400).send(result.error.details[0].message)
-        return
-    }
     try {
-        let file = await fileStore.findOne({"name": req.body.name});
-        const isFileNew = !file
-        if (isFileNew) {
-            // File doesn't exist, so create a new one
-            file = new fileStore(req.body);
-        } else {
-            // File exists, update its content
-            file.content = req.body.content;
-        }
-
-        await file.save()
-
-        if (isFileNew) {
-            // If the file is new, send a message indicating that it's created
-            res.send(`File did not exist in store. Created file ${req.body.name}`);
-        } else {
-            // If the file already existed, send message accordingly
-            res.send(`Updated file ${req.body.name}`);
-        }
+        console.log(`Updated file ${req.file.originalname}`)
+        res.send(`Updated file ${req.file.originalname}`)
     } catch (error) {
         console.log(error.message);
         res.status(500).send(`Error ${error.message}`);
@@ -129,5 +133,38 @@ app.get('/wc', (req, res) => {
 app.get('/freq-words', (req, res) => {
 
 })
+
+
+// Utility functions
+
+// Get a file by filename
+const getFileByFilename = async (filename) => {
+    try {
+        return await gfs.files.findOne({ filename });
+    } catch (err) {
+        throw err;
+    }
+};
+
+//Delete a file by filename: returns 1 : successful, 0 : failure
+const deleteFileByFilename = async (filename) => {
+    try {
+        console.log(filename)
+        const file = await gfs.files.findOne({ filename });
+        // check if file exists in file store
+        if(!file){
+            console.log('check 1')
+            console.log(file)
+            return 0
+        }
+        const gsfb = new mongoose.mongo.GridFSBucket(conn.db, { bucketName: 'uploads' });
+        gsfb.delete(file._id);
+        return 1
+    } catch (error) {
+        console.log(error.message)
+        console.log('check 2')
+        return 0
+    }
+}
 
 app.listen(5000, () => console.log('Listening on port 5000...'))
